d26f218f01389591753e97be3ba66b34260ae18f
Delold <cpdavidduong@gmail.com>
2014-05-06 12:22:40 +0200

    Fixing Gruntfile extraction of nw

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,17 @@
 module.exports = function(grunt) {
+	var date 		= new Date(),
+		version 	= "0.8.5",
+		nw_file 	= "";
 
-	var date = new Date();
-
-	var version = "0.8.5";
-
-	var request = require("request");
-	var _ = require('lodash');
-	var fs = require("fs");
-	var progressbar = require("progress");
-	var path = require("path");
-	var q = require("q");
-	var admzip = require("adm-zip");
-
-	var zlib = require("zlib");
-	var tar = require("tar-fs");
+	var request 	= require("request"),
+		_ 			= require('lodash'),
+		fs 			= require("fs"),
+		progressbar = require("progress"),
+		path 		= require("path"),
+		q 			= require("q"),
+		zlib		= require("zlib"),
+		tar 		= require("tar-fs"),
+		zip 		= require("zip");
 
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-shell');
@@ -22,7 +20,26 @@ module.exports = function(grunt) {
 
 	var settings = {
 		filename: ("0"+date.getDate()).slice(-2) + ("0"+(date.getMonth()+1)).slice(-2) + date.getFullYear().toString() + ("0"+date.getHours()).slice(-2) + ("0"+date.getMinutes()).slice(-2) + ("0"+date.getSeconds()).slice(-2),
-		download_url: 'http://dl.node-webkit.org/'
+		download_url: 'http://dl.node-webkit.org/',
+		platform_options: [{
+			'url': "v<%= version %>/node-webkit-v<%= version %>-win-ia32.zip",
+			'type': 'win32',
+			'files': ['ffmpegsumo.dll', 'icudt.dll', 'libEGL.dll', 'libGLESv2.dll', 'nw.exe', 'nw.pak'],
+			'exclude': ['nwsnapshot.exe']
+		}, {
+			'url': "v<%= version %>/node-webkit-v<%= version %>-osx-ia32.zip",
+			'type': 'darwin',
+			'files': ['node-webkit.app'],
+			'exclude': ['nwsnapshot']
+		}, {
+			'url': "v<%= version %>/node-webkit-v<%= version %>-linux-ia32.tar.gz",
+			'type': 'linux',
+			'files': ['nw', 'nw.pak', 'libffmpegsumo.so'],
+			'exclude': ['nwsnapshot']
+		}],
+		file_list: ["src/**", "!src/builds/**", "!src/node_modules/**", "!src/bower.json", "!src/*.s3db", "!src/*.sublime-project", "!src/*.sublime-workspace", "!src/*.log", "!src/assets/less/**",
+					"src/node_modules/sqlite3/LICENSE", "src/node_modules/sqlite3/package.json", "src/node_modules/sqlite3/sqlite3.js", "src/node_modules/sqlite3/lib/**", "src/node_modules/sqlite3/node_modules/**"]
+					.map(function(item) { return item.replace(/\//g, path.sep); })
 	};
 
 	var platform_options = [{
@@ -41,25 +58,19 @@ module.exports = function(grunt) {
 		'files': ['nw', 'nw.pak', 'libffmpegsumo.so'],
 		'exclude': ['nwsnapshot']
 	}];
-
-	var file_list = ["src/**", "!src/builds/**", "!src/node_modules/**", "!src/bower.json", "!src/*.s3db", "!src/*.sublime-project", "!src/*.sublime-workspace", "!src/*.log", "!src/assets/less/**",
-		"src/node_modules/sqlite3/LICENSE", "src/node_modules/sqlite3/package.json", "src/node_modules/sqlite3/sqlite3.js", "src/node_modules/sqlite3/lib/**", "src/node_modules/sqlite3/node_modules/**"]
-		.map(function(item) { return item.replace(/\//g, path.sep); });
 	
-	var nw_file;
-	var options = platform_options[_.findIndex(platform_options, {type: process.platform})];
-
-	// var options = platform_options[2];
+	
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		src_pkg: grunt.file.readJSON(path.join('src', 'package.json')),
 		compress: {
 			zip: {
 				options: {
 					archive: path.join('.', 'builds', settings.filename+'.zip'),
 					mode: "zip"
 				},
-				files: [{ src: file_list }]
+				files: [{ src: settings.file_list }]
 			}
 		},
 		shell: {
@@ -78,14 +89,16 @@ module.exports = function(grunt) {
 	});
 	
 
-	grunt.registerTask("nw-download", "downloads node-webkit", function() {
-		var done = this.async();
+	grunt.registerTask("nw-download", "Downloads and extracts node-webkit", function() {
+		var done 	= this.async();
+		var dest 	= "src";
+		var options = settings.platform_options[_.findIndex(settings.platform_options, {type: process.platform})];
 		
-		var url = grunt.template.process(options.url, {data: {"version": version}});
-		nw_file = path.join("temp", url.split("/").slice(-1).toString());
+		var url 	= grunt.template.process(options.url, {data: {"version": version}});
+			nw_file = path.join("temp", url.split("/").slice(-1).toString());
 
-		if (!fs.existsSync("temp")) {
-			fs.mkdirSync("temp");
+		if (!fs.existsSync(dest)) {
+			fs.mkdirSync(dest);
 		}
 
 		var out = fs.createWriteStream(nw_file);
@@ -95,7 +108,7 @@ module.exports = function(grunt) {
 		srv.on("response", function(res) {
 			var len = parseInt(res.headers['content-length'], 10);
 
-			var bar = new progressbar('Stahuji'["red"]+' [:bar] :percent :etas', {
+			var bar = new progressbar('Stahuji'["magenta"]+' [:bar] :percent :etas', {
 				complete: '=',
 				incomplete: ' ',
 				width: 30,
@@ -107,21 +120,32 @@ module.exports = function(grunt) {
 			});
 
 			srv.on("end", function() {
-				grunt.log.writeln("Staženo, rozbaluji"["blue"]);
+				grunt.log.writeln("Staženo, rozbaluji"["green"]);
 
 				if(path.extname(nw_file) === ".zip") {
+					var zipReader = zip.Reader(fs.readFileSync(nw_file));
 
-					var zip = new admzip(nw_file);
+					zipReader.forEach(function(entry) {				
+						var mode = entry.getMode(), shortpath = path.join(dest, entry.getName()), filepath = path.resolve(shortpath);
 
-					zip.getEntries().forEach(function(entry) {
-						if(options.files.indexOf(entry.name) > -1) {
-							zip.extractEntryTo(entry.entryName, "src", true, true);
+						if(_.intersection(shortpath.split(path.sep), options.files).length > 0) {
+							console.log("Rozbaluji soubor: "["grey"]+path.basename(filepath)["green"]);
+							if(entry.isDirectory()) {
+								grunt.file.mkdir(filepath, function(err) {
+									if(mode) { fs.chmodSync(filepath, mode); }
+								});
+							} else {
+								grunt.file.mkdir(path.dirname(filepath));
+								fs.writeFileSync(filepath, entry.getData());
+
+								if(mode) { fs.chmodSync(filepath, mode); }
+							}
 						}
 					});
 
 					done(true);
 
-				} else if (path.extname(nw_file) === ".gz") {
+				} else {
 					fs.createReadStream(nw_file)
 						.pipe(zlib.createGunzip())
 						.pipe(tar.extract("temp"))
@@ -130,29 +154,29 @@ module.exports = function(grunt) {
 
 							fs.readdir(path.join("temp", basename), function(err, files) {
 								_.intersection(files, options.files).forEach(function(file) {
+
+									console.log("Kopíruji soubor: "["grey"]+path.basename(file)["green"]);
+
 									fs.renameSync(path.join("temp", basename, file), 
-												  path.join("src", file));
+												  path.join(dest, file));
 								});
 
 								done(true);
 							});
-					});
-				}
-
-				
+						});
+				}	
 			});
 		});
 
 		srv.pipe(out);
 	});
 
-	grunt.registerTask("verify-structure", "tries, if everything is alright", function() {
+	grunt.registerTask("verify-structure", "Checks, if the server address is set correctly", function() {
 		var file = grunt.file.read(path.join("src", "assets", "scripts", "defaults.js"), {encoding:"UTF8"});
 
 		grunt.log.writeln();
-		grunt.log.writeln("Verze balíčku: " + grunt.config.get("pkg.version")["green"]);
-		grunt.log.writeln("Název souboru: " + (settings.filename+".zip")["green"]);
-		grunt.log.writeln();
+		grunt.log.writeln("Verze balíčku: "["cyan"] + grunt.config.get("src_pkg.version")["green"]);
+		grunt.log.writeln("Název souboru: "["cyan"] + (settings.filename+".zip")["green"]);
 
 		if(file.match(/var\s+host\s*=\s*"duong\.cz"/) == null) {
 			grunt.fail.warn("Není nastaven správě host na server");
@@ -160,6 +184,6 @@ module.exports = function(grunt) {
 	});
 
 	grunt.registerTask("init", ["nw-download", "shell", "clean"]);
-	grunt.registerTask('default', ['verify-structure', 'compress']);
-
+	grunt.registerTask('dist', ['verify-structure', 'compress']);
+	grunt.registerTask('default', ['verify-structure']);
 };
